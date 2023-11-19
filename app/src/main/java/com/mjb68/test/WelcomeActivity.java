4eff31f3b91bf647d89326114a809b75726f6135
lihui <973742475@qq.com>
2023-11-19 11:56:58 +0800

    try

--- app/src/main/java/com/mjb68/test/WelcomeActivity.java
+++ app/src/main/java/com/mjb68/test/WelcomeActivity.java
@@ -43,37 +43,41 @@ public class WelcomeActivity extends AppCompatActivity {
                     @Override
                     public void onComplete(@NonNull Task<Boolean> task) {
                         //url、key、force2B 自行aes、des加密解密，不要使用明文。防止封号
-                        String adjust_key = mFirebaseRemoteConfig.getString(AdjustEventModel.adjust_key);
-                        app_url = mFirebaseRemoteConfig.getString(AdjustEventModel.app_url_key);
+                        try {
+                            String adjust_key = mFirebaseRemoteConfig.getString(AdjustEventModel.adjust_key);
+                            app_url = mFirebaseRemoteConfig.getString(AdjustEventModel.app_url_key);
 
-                        if (!TextUtils.isEmpty(adjust_key)){
-                            register_success = mFirebaseRemoteConfig.getString(AdjustEventModel.register_success_key);
-                            recharge_success = mFirebaseRemoteConfig.getString(AdjustEventModel.recharge_success_key);
-                            first_recharge_success = mFirebaseRemoteConfig.getString(AdjustEventModel.first_recharge_success_key);
-                            MyAdjustUtils.init(adjust_key);
+                            if (!TextUtils.isEmpty(adjust_key)) {
+                                register_success = mFirebaseRemoteConfig.getString(AdjustEventModel.register_success_key);
+                                recharge_success = mFirebaseRemoteConfig.getString(AdjustEventModel.recharge_success_key);
+                                first_recharge_success = mFirebaseRemoteConfig.getString(AdjustEventModel.first_recharge_success_key);
+                                MyAdjustUtils.init(adjust_key);
+                            }
+
+                            Log.d(TAG, "adjust_key: " + adjust_key);
+                            Log.d(TAG, "app_url: " + app_url);
+                            Log.d(TAG, "register_success: " + register_success);
+                            Log.d(TAG, "recharge_success: " + recharge_success);
+                            Log.d(TAG, "first_recharge_success: " + first_recharge_success);
+
+                            Toast.makeText(WelcomeActivity.this,
+                                    "adjust_key: " + adjust_key + ",app_url: " + app_url +
+                                            ",register_success: " + register_success + ",recharge_success: " + recharge_success +
+                                            ".first_recharge_success: " + first_recharge_success, Toast.LENGTH_LONG).show();
+
+
+                            //先不做归因，直接接口返回有值，就跳转
+                            if (TextUtils.isEmpty(app_url)) {
+                                startActivity(new Intent(WelcomeActivity.this, A_Activity.class));
+                            } else {
+                                startActivity(new Intent(WelcomeActivity.this, B_WebActivity.class));
+                            }
+
+                            finish();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                            Log.e(TAG, "e:" + e.getMessage());
                         }
-
-                        Log.d(TAG, "adjust_key: " + adjust_key);
-                        Log.d(TAG, "app_url: " + app_url);
-                        Log.d(TAG, "register_success: " + register_success);
-                        Log.d(TAG, "recharge_success: " + recharge_success);
-                        Log.d(TAG, "first_recharge_success: " + first_recharge_success);
-
-                        Toast.makeText(WelcomeActivity.this,
-                                "adjust_key: " + adjust_key + ",app_url: " + app_url +
-                                        ",register_success: " + register_success + ",recharge_success: " + recharge_success +
-                                        ".first_recharge_success: " + first_recharge_success, Toast.LENGTH_LONG).show();
-
-
-                        //先不做归因，直接接口返回有值，就跳转
-                        if (TextUtils.isEmpty(app_url)) {
-                            startActivity(new Intent(WelcomeActivity.this, A_Activity.class));
-                        } else {
-                            startActivity(new Intent(WelcomeActivity.this, B_WebActivity.class));
-                        }
-
-                        finish();
-
                     }
                 });
     }
